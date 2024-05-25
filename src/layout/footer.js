6b514289a505afbf11dc411789427ff742c61df1
Juan Diego CJ <166154528+JuanDiegoCJ@users.noreply.github.com>
2024-05-25 03:28:48 +0000

    commit1

--- src/layout/footer.js
+++ src/layout/footer.js
@@ -1,17 +1,11 @@
 const Footer = () => {
-    return <footer data-bs-theme="white" className="bd-footer py-4 py-md-5 bg-body-tertiary">
-        <div className="row">
-                    <div className="col-md-1">
-                    <i data-feather="facebook"></i>
-                    </div>
-                    <div className="col-md-1">
-                    <i data-feather="instagram"></i>
-                    </div>
-                    <div className="col-md-1">
-                    <i data-feather="twitter"></i>
-                    </div>
-                </div> 
-    </footer>;
+    return (
+    <footer className="footerStyle">
+        <p> <h1> <i data-feather="facebook"></i> <i data-feather="instagram"></i> <i data-feather="twitter"></i>
+            </h1> </p>
+    </footer>
+);
+
 }
 
 export default Footer;
